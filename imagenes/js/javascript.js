f8c42a3f16ed54edae5de07945e0a693799f65b9
domerchan <domerchan@users.noreply.github.com>
2019-05-19 23:35:20 -0500

    banner de imágenes

---
+++ imagenes/js/javascript.js
@@ -0,0 +1,42 @@
+var slideIndex = 1;
+showDivs(slideIndex);
+
+window.onload = function() {
+	iniciar();
+}
+
+function iniciar() {
+	slideIndex = 1;
+	showDivs(1);
+	array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+	var val;
+	for(i = 0; i < 5; i++) 
+		array.splice(Math.floor(Math.random() * array.length), 1);
+
+	for(i = 0; i < 5; i++) 
+		document.getElementById(i).src = "img/"+ array[i] +".jpg";
+}
+
+function plusDivs(n) {
+	showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+	var i;
+	var x = document.getElementsByClassName("mySlides");
+	if (n == x.length)
+		document.getElementById('right').disabled = true;
+	else
+		document.getElementById('right').disabled = false;
+
+	if (n == 1) 
+		document.getElementById('left').disabled = true;
+	else
+		document.getElementById('left').disabled = false
+
+	for (i = 0; i < x.length; i++) {
+	x[i].style.display = "none"; 
+	}
+
+	x[slideIndex-1].style.display = "block"; 
+}
